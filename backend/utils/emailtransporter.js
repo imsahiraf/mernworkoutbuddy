93f97283d65c24fab0f10a0e0e312e37b36ba254
imsahiraf <zulfekarshaikh0@gmail.com>
2023-02-11 15:55:49 +0530

    Class base Mailing with O/C Principle

--- backend/utils/emailtransporter.js
+++ backend/utils/emailtransporter.js
@@ -1,32 +1,33 @@
 require('dotenv').config();
 
 module.exports = () => {
-    if(process.env.mailservice == 'gmail'){
-        return gmailtransport()
-    }else{
-        return mailtraptransport()
-    }
-}
+    const transportor = process.env.mailservice+'transport'
+    return eval(transportor).transport()
+}   
 
-const gmailtransport = () => {
-    return {
-        service: 'gmail',
-        secure: false,
-        port: 25,
-        auth: {
-            user: 'testmailerjulf@gmail.com',
-            pass: 'hbkvnxzcojndvayo'
+class gmailtransport {
+    static transport () { // Calling it statically so we dont have to create an instance
+        return {
+            service: 'gmail',
+            secure: false,
+            port: 25,
+            auth: {
+                user: 'testmailerjulf@gmail.com',
+                pass: 'hbkvnxzcojndvayo'
+            }
         }
     }
 }
 
-const mailtraptransport = () => {
-    return {
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: "b6f01b7d916642",
-            pass: "b637db19d2f47c"
+class mailtraptransport {
+    static transport () {
+        return {
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: "b6f01b7d916642",
+                pass: "b637db19d2f47c"
+            }
         }
     }
 }
